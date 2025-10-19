179bd7bfbeab94632db987698b95c079439885ce
ceciliavlz <mariacvelzi@gmail.com>
2025-10-19 13:00:48 -0300

    script modificado para mejor depuración

--- compilar.bat
+++ compilar.bat
@@ -1,47 +1,71 @@
+@echo off
+cls
+
 REM Crear carpeta bin si no existe
 if not exist bin mkdir bin
 
-@echo off
-cls
 echo ==========================================
 echo        COMPILANDO UNITS...
 echo ==========================================
-REM Compilar el units
-fpc -FUbin -Fu src\model\envio.pas
+
+echo Compilando envio.pas...
+fpc src\model\envio.pas -FUbin
+if errorlevel 1 goto Error
+
 echo ==========================================
-fpc -FUbin -Fu src\utils\DAOUtils.pas
+echo Compilando DAOUtils.pas...
+fpc src\utils\DAOUtils.pas -FUbin
+if errorlevel 1 goto Error
+
 echo ==========================================
-fpc -FUbin -Fu src\dao\DAOenvio.pas
+echo Compilando DAOenvio.pas...
+fpc src\dao\DAOenvio.pas -FUbin
+if errorlevel 1 goto Error
+
 echo ==========================================
-fpc -FUbin -Fu src\controller\ControllerEnvio.pas
+echo Compilando ControllerEnvio.pas...
+fpc src\controller\ControllerEnvio.pas -FUbin
+if errorlevel 1 goto Error
+
 echo ==========================================
-fpc -FUbin -Fu src\view\menu.pas
+echo Compilando menu.pas...
+fpc src\view\menu.pas -FUbin
+if errorlevel 1 goto Error
 echo.
 
-@echo off
 echo ==========================================
 echo        COMPILANDO PROGRAMA PRINCIPAL...
 echo ==========================================
-REM Compilar el programa
 fpc src\main.pas -FEbin -obin\TPF.exe
+if errorlevel 1 goto Error
 echo.
 
-REM Verificar si la compilacion fue exitosa
-if %errorlevel%==0 (
-    @echo off
-    echo ==========================================
-    echo        LIMPIANDO ARCHIVOS TEMPORALES ...
-    echo ==========================================
-    echo.
-    del /q bin\*.o bin\*.ppu 2>nul
-    echo ==========================================
-    echo  El programa se ha compilado correctamente.
-    echo  Puedes ejecutarlo con: bin\TPF.exe
-    echo ==========================================
-) else (
-    echo.
-    echo ERROR: La compilacion ha fallado.
-)
+echo ==========================================
+echo  El programa se ha compilado correctamente.
+echo  Puedes ejecutarlo con: bin\TPF.exe
+echo ==========================================
+goto LimpiarArchivos
+echo.
 
+:Error
+color 4F
+echo.
+echo ==========================================
+echo  ERROR: La compilacion ha fallado.
+echo ==========================================
 echo.
 pause
+goto LimpiarArchivos
+
+:LimpiarArchivos
+echo ==========================================
+echo        LIMPIANDO ARCHIVOS TEMPORALES ...
+echo ==========================================
+del /q bin\*.o bin\*.ppu 2>nul
+goto End
+echo.
+
+:End
+echo.
+pause
+if errorlevel 1 exit /b 1 else exit /b 0
